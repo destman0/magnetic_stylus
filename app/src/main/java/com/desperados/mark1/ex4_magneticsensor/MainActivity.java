6ac858ba3b6bec3ce22862906403bfded63b0bb6
markeisenberg <markeisenberg43@gmail.com>
2016-05-28 13:05:41 +0200

    Initial Android Studio Commit

---
+++ app/src/main/java/com/desperados/mark1/ex4_magneticsensor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.desperados.mark1.ex4_magneticsensor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/desperados/mark1/ex4_magneticsensor/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.desperados.mark1.ex4_magneticsensor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
