c38d34095615a82da6789ea57540bfe40923cea2
dest_0 <dest_0@tut.by>
2016-06-05 15:12:14 +0200

    Magnetic sensor enabled, trying to figure out coordinate system

--- app/src/main/java/com/desperados/mark1/ex4_magneticsensor/MainActivity.java
+++ app/src/main/java/com/desperados/mark1/ex4_magneticsensor/MainActivity.java
@@ -2,10 +2,21 @@ package com.desperados.mark1.ex4_magneticsensor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.Toast;
+import android.content.Context;
 
-public class MainActivity extends AppCompatActivity {
+import 	java.lang.Math;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container, new ShapeFragment())
                     .commit();
         }
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        } else {
+            Toast.makeText(getApplicationContext(), "No accelerometer found", Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 
     @Override
@@ -35,4 +58,32 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+
+        float bx, by, bz, xa, ya, r;
+        //Log.i("X", ""+event.values[0]);
+        //Log.i("Y", ""+event.values[1]);
+        //Log.i("Z", ""+event.values[2]);
+
+        bx = event.values[0];
+        by = event.values[1];
+        bz = event.values[2];
+        //xa = r*Math.cos(Math.atan(by/bx));
+        //ya = r*Math.sin(Math.atan(by/bx));
+
+
+
+    }
+
 }
+
+
+
